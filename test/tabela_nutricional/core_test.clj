a4a328c36d25fa6e02cd4c6998fcd8872349f667
Pedro Vieira <160655744+PedroVSX@users.noreply.github.com>
2025-05-16 19:42:43 -0300

    Setting up master

---
+++ test/tabela_nutricional/core_test.clj
@@ -0,0 +1,7 @@
+(ns tabela-nutricional.core-test
+  (:require [clojure.test :refer :all]
+            [tabela-nutricional.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
