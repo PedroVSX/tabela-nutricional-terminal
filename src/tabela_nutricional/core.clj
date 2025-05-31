69fa167e5bdd744337836a0e490cf1dc0263e3ad
BrauleFernandes <braulefernandes@yahoo.com>
2025-05-31 09:11:21 -0300

    Atualizações nos textos do terminal

--- src/tabela_nutricional/core.clj
+++ src/tabela_nutricional/core.clj
@@ -7,30 +7,29 @@
 ;;Responsável por "enviar" os dados/requisições
 
 (defn salvar-dados-usuario [nome altura peso idade sexo]
-  (let [dados-usuario {:nome nome
-               :altura altura
-               :peso peso
-               :idade idade
-               :sexo sexo}]
+  (let [dados-usuario {:nome   nome
+                       :altura altura
+                       :peso   peso
+                       :idade  idade
+                       :sexo   sexo}]
+    ;;(println (str "\n Dados do usuario salvos: " dados-usuario "\n"))
 
-      (println (str "\n Dados do usuario salvos: " dados-usuario "\n"))
+    (try (http/post "http://localhost:3000/usuarios"
+                    {:body    (json/encode dados-usuario)
+                     :headers {"Content-Type" "application/json"}})
 
-      (try (http/post "http://localhost:3000/usuarios"
-                   {:body (json/encode dados-usuario)
-                    :headers {"Content-Type" "application/json"}})
+         (println "\nCadastro concluido com sucesso!")
 
-        (println "Cadastro concluido com sucesso!")
-
-        (catch Exception e
-          (println "Nao foi possivel concluir seu registro de consumo, pois" (.getMessage e))))))
+         (catch Exception e
+           (println "Nao foi possivel concluir seu registro de consumo, pois" (.getMessage e))))))
 
 (defn salvar-dados-consumo [alimento quantidade]
-  (let [dados-consumo {:alimento alimento
-               :quantidade quantidade}]
+  (let [dados-consumo {:alimento   alimento
+                       :quantidade quantidade}]
     (println (str "\n Dados do consumo salvos: " dados-consumo "\n"))
 
     (try (http/post "http://localhost:3000/COLOCAR O CAMINHO PARA SALVAR OS DADOS DO USUARIO"
-                    {:body (json/encode dados-consumo)
+                    {:body    (json/encode dados-consumo)
                      :headers {"Content-Type" "application/json"}})
 
          (println "Consumo registrado com sucesso!")
@@ -40,11 +39,11 @@
 
 (defn salvar-dados-atividade [atividade tempo]
   (let [dados-atividade {:atividade atividade
-               :tempo tempo}]
+                         :tempo     tempo}]
     (println (str "\n Dados da atividade salvos: " dados-atividade "\n"))
 
     (try (http/post "http://localhost:3000/COLOCAR O CAMINHO PARA SALVAR OS DADOS DO USUARIO"
-                    {:body (json/encode dados-atividade)
+                    {:body    (json/encode dados-atividade)
                      :headers {"Content-Type" "application/json"}})
 
          (println "Registro de atividade feito com sucesso!")
@@ -57,7 +56,7 @@
 (defn buscar-dados-usuario []
   (try
     (let [resposta-busca (http/get "http://localhost:3000/usuarios/1"
-                             {:headers {"Accept" "application/json"}})
+                                   {:headers {"Accept" "application/json"}})
           dados-usuario (json/parse-string (:body resposta-busca) true)] ; true => converte para keywords
 
       ;; Mostra os dados no terminal
@@ -68,7 +67,8 @@
       (println "Sexo:" (:sexo dados-usuario)))
 
     (catch Exception e
-      (println "Erro ao buscar dados do usuário:" (.getMessage e)))))
+      (println "Nao foi posssivel encontrar os dados do usuario")
+      (println "Por conta do erro:" (.getMessage e)))))
 
 (defn buscar-dados-consumo []
   (try
@@ -96,7 +96,9 @@
     (catch Exception e
       (println "Erro ao buscar dados de atividade:" (.getMessage e)))))
 
+
 (defn menu-cliente []
+  (println "===============================================================")
   (print (str
            "Seja bem vindo a nossa Calculadora\n"
            "Ja e nosso cliente?\n"
@@ -108,14 +110,16 @@
   )
 
 (defn consultar-dados []
-  (println "\n============= Dados Pessoais =============")
+  (println "\n======================= Dados Pessoais ========================")
   (buscar-dados-usuario)
 
-  (print "Digite 0 para retornar ao menu anterior: ") (flush)
+  (print "\nDigite 0 para retornar ao menu anterior: ") (flush)
   (let [opcao (read)]
+    (println "===============================================================")
     (cond
-      (= opcao 0) (println "Voltando ao menu principal...")
-      :else (do (println "Opcao invalida.") (recur)))))
+
+      (= opcao 0) (println "\nVoltando ao menu principal...")
+      :else (do (println "\nOpcao invalida...") (recur)))))
 
 (defn registrar-consumo []
   (print "Digite o nome do alimento: ") (flush)
@@ -138,7 +142,7 @@
   (println "Seu saldo de calorias é: SALDO DE CALORIAS"))
 
 (defn menu-cliente-cadastrado []
-  (println "\n--- Menu Cliente Cadastrado ---")
+  (println "\n====================== Menu de Operacoes ======================")
   (println "1 - Consultar dados pessoais (altura, peso, idade e sexo).")
   (println "2 - Registrar consumo de alimento (ganho de caloria).")
   (println "3 - Registrar realizacao de atividade fisica (perda de caloria).")
@@ -147,6 +151,7 @@
   (println "6 - Voltar.")
   (print "Escolha uma opcao: ") (flush)
   (let [opcao (read)]
+  (println "===============================================================\n")
     (cond
       (= opcao 1) (do (consultar-dados) (recur))
       (= opcao 2) (do (registrar-consumo) (recur))
@@ -157,6 +162,7 @@
       :else (do (println "Opcao invalida.") (recur)))))
 
 (defn cadastrar-usuario []
+  (println "\n======== Informe os dados abaixo =========")
   (print "Digite seu nome: ") (flush)
   (let [nome (read)]
     (print "Digite sua altura: ") (flush)
@@ -167,15 +173,18 @@
         (let [idade (read)]
           (print "Diga seu sexo: ") (flush)
           (let [sexo (read)]
-            (salvar-dados-usuario nome altura peso idade sexo)))))))
+            (salvar-dados-usuario nome altura peso idade sexo))))))
+  (println "==========================================\n")
+  )
 
 (defn menu-cliente-nao-cadastrado []
-  (println "\n--- Menu Cadastro ---")
+  (println "\n============ Menu de Cadastro ============")
   (println "1 - Registrar novo usuario")
   (println "2 - Voltar")
   (print "Escolha uma opcao: ")
   (flush)
   (let [opcao (read)]
+    (println "==========================================")
     (cond
       (= opcao 1) (cadastrar-usuario)
       (= opcao 2) (println "Voltando ao menu principal...")
